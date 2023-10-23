f34840886c461bd6315c9a851fa2b9cf4e71ef12
Grinchs <marcisg2345@gmail.com>
2023-10-23 23:26:18 +0300

    Add project files.

---
+++ Nosacijuma_operatori_4-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Ievadiet skaitli: ");
+        int skaitlis = int.Parse(Console.ReadLine());
+
+        if (skaitlis % 2 == 0)
+        {
+            Console.WriteLine(skaitlis + " ir pāra skaitlis.");
+        }
+        else
+        {
+            Console.WriteLine(skaitlis + " ir nepāra skaitlis.");
+        }
+    }
+}
